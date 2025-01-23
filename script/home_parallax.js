780520017fde2e34fff47cb53a7839c1defe8087
EndPlay <128982259+ReeveWorks@users.noreply.github.com>
2025-01-23 18:11:59 +0800

    Update 0.1.3 Fix Train Parallax
    - removed img element and css
    - change train image location back to background-image
    - change parallax code to target the css instead of the html

--- script/home_parallax.js
+++ script/home_parallax.js
@@ -1,29 +1,44 @@
 //let handrails = document.getElementById('id-handrails');
 
-//let hill1 = document.getElementById('hill1');
-//let hill2 = document.getElementById('hill2');
-//let hill3 = document.getElementById('hill3');
-//let hill4 = document.getElementById('hill4');
-//let hill5 = document.getElementById('hill5');
-
 let txthome = document.getElementById('txthome');
-let railshome = document.getElementById('railshome');
+let rails = document.getElementById('railshome');
 let train = document.getElementById('trainhome');
+let towers = document.getElementById('towershome');
+let sea = document.getElementById('seascape');
 
 window.addEventListener('scroll', ()=> {
     let value = window.scrollY;
 
-    txthome.style.marginBottom= value * -1.5 + 'px';
-
-    //railshome.style.left = value * -0.5 + 'px';
-    train.style.left = value * 5 + 'px';
-
-    //leaf.style.top = value * -1.5 + 'px';
-    //leaf.style.left = value * 1.5 + 'px';
+    document.querySelector('.home-train').style.backgroundPosition = `calc(${value * 3.5 - 2150}px) bottom`;
+    //document.querySelector('.home-seascape').style.backgroundPosition = `calc(${value * -2}px) bottom`;
     
-    //handrails.style.left = value * 1.5 + 'px';
-    //hill4.style.left = value * -1.5 + 'px';
-    //hill1.style.top = value * 1 + 'px';
-
-
 })
+
+//Script Notes
+
+/*
+    train.src = '/resources/home/train.png';
+    train.style.width = 'auto';
+    train.style.display = 'block';
+    train.style.height = '380px';
+    train.style.marginLeft = '0%';
+    train.style.position = 'absolute';
+    train.style.bottom = '0';
+
+    train.style.left = value * 2 + 'px';
+    
+    txthome.style.marginBottom= value * -1.5 + 'px';
+    railshome.style.left = value * -0.5 + 'px';
+    
+    train.style.bottom = value * -0.25 + 'px';
+
+    towers.style.bottom = value * -0.7 + 'px';
+    sea.style.bottom = value * -0.5 + 'px';
+
+    leaf.style.top = value * -1.5 + 'px';
+    leaf.style.left = value * 1.5 + 'px';
+    
+    handrails.style.left = value * 1.5 + 'px';
+    hill4.style.left = value * -1.5 + 'px';
+    hill1.style.top = value * 1 + 'px';
+*/
